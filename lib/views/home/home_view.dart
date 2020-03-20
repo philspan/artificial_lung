71bf63c4c5008218168720f84bc529ce50705dc6
philspan <philspan@umich.edu>
2020-03-20 12:12:15 -0400

    added graph library and fixed formatting

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,6 @@
+import 'package:bezier_chart/bezier_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+// import 'package:charts_flutter/flutter.dart' as charts;
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
@@ -7,27 +8,33 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        Flexible(child: FractionallySizedBox(heightFactor: .3)),
-        Flexible(
-          child: FractionallySizedBox(
-            heightFactor: .5,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GraphContainer(),
-            ),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(8, 42, 8, 8),
+            child: WelcomeContainer(),
           ),
-        ),
-        Flexible(
-          child: FractionallySizedBox(
-            heightFactor: .8,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: BluetoothConnectionContainer(),
-            ),
+        GraphContainer(),
+        AspectRatio(
+          aspectRatio: 1 / .47,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BluetoothConnectionContainer(),
           ),
         ),
       ],
+    );
+  }
+}
+
+class WelcomeContainer extends StatelessWidget {
+  const WelcomeContainer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Text("", style: TextStyle(fontSize: 24)),
     );
   }
 }
@@ -97,21 +104,40 @@ class GraphContainer extends StatelessWidget {
               style: TextStyle(),
             ),
           ),
-          charts.BarChart([]),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: BezierChart(
+                bezierChartScale: BezierChartScale.CUSTOM,
+                xAxisCustomValues: const [0, 5, 10, 15, 20, 25, 30, 35],
+                series: const [
+                  BezierLine(
+                    data: const [
+                      DataPoint<double>(value: 10, xAxis: 0),
+                      DataPoint<double>(value: 130, xAxis: 5),
+                      DataPoint<double>(value: 50, xAxis: 10),
+                      DataPoint<double>(value: 150, xAxis: 15),
+                      DataPoint<double>(value: 75, xAxis: 20),
+                      DataPoint<double>(value: 0, xAxis: 25),
+                      DataPoint<double>(value: 5, xAxis: 30),
+                      DataPoint<double>(value: 45, xAxis: 35),
+                    ],
+                  ),
+                ],
+                config: BezierChartConfig(
+                  verticalIndicatorStrokeWidth: 3.0,
+                  verticalIndicatorColor: Colors.black26,
+                  showVerticalIndicator: true,
+                  backgroundColor: Colors.red,
+                  snap: false,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-/*
-
-            charts.BarChart(
-              [
-                charts.Series(
-                    id: "",
-                    data: [],
-                    domainFn: (datum, int index) {},
-                    measureFn: (datum, int index) {})
-              ],
-            ),*/
