7a9608802b3bf7b99d57667e2ad80473eab60d55
Phil Spannagel <philspan@umich.edu>
2020-06-03 14:48:32 -0400

    Modified DataService to give onChange for data using ReactiveServiceMixin (observable_ish)

--- lib/core/services/data.dart
+++ lib/core/services/data.dart
@@ -1,28 +1,33 @@
 import 'package:artificial_lung/core/models/data.dart';
 import 'package:artificial_lung/core/services/storage.dart';
 import 'package:artificial_lung/locator.dart';
+import 'package:observable_ish/observable_ish.dart';
+import 'package:stacked/stacked.dart';
 
-class DataService {
+class DataService with ReactiveServiceMixin {
   final _storage = locator<Storage>();
 
   // possibly change to a stream of Data
-  List<Datum> _data;
-  List<Datum> get data => _data;
+  RxValue<List<Datum>> _data = RxValue<List<Datum>>();
+  // RxList<Datum> _data = RxList<Datum>();
+  List<Datum> get data => _data.value;
 
-  bool get hasData => _data != null && _data.isNotEmpty;
+  bool get hasData => _data.value != null && _data.value.isNotEmpty;
 
   DataService() {
     fetchData();
+    listenToReactiveValues([_data]);
   }
 
   // add functions to handle getting data from list indirectly
   // TODO create getters for parts of data, rather than requiring use of dot operator
   Datum get first {
-    return _data.first;
+    return _data.value.first;
   }
-  
+
   Future<List<Datum>> fetchData() async {
-    _data = await _storage.readDataFromFile();
-    return _data;
+    // _data = RxList.from(await _storage.readDataFromFile());
+    _data.value = await _storage.readDataFromFile();
+    return _data.value;
   }
 }
